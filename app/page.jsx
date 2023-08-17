c70840aba2b33a3ea4b169c0def644510ecc8501
John Geliberte <gelibertejohn@gmail.com>
2023-08-17 20:04:30 +0800

    + Added landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
+
 const Landing = () => {
   return (
     <section className="w-full flex-center flex-col">
-
+        <h1 className="head_text text-center">
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">Community Based Early Warning System for Landslides</span>
+        </h1>
+        <p className="desc text-center">
+            {/* Empowering Safety: Uniting Communities with Timely Landslide Alerts */}
+            Brgy. Poblacion, Bakun, Benguet
+        </p>
     </section>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,71 @@
 'use client'
 
+import PDRRMO_SEAL from '@public/images/pdrrmo_seal.png';
+import MDRRMO_SEAL from '@public/images/mdrrmo_seal.png';
+import BRGY_SEAL from '@public/images/brgy_seal.png';
+import DOST_SEAL from '@public/images/dost_seal.png';
+import DYNASLOPE_SEAL from '@public/images/dynaslope_seal.png';
+
 import { useEffect, useState } from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+        <Link href="/" className="flex gap-3 flex-center">
+            <Image 
+                src={DYNASLOPE_SEAL}
+                alt="Dynaslope seal"
+                width={75}
+                height={75}
+                className="object-contain"
+            />
+            <Image 
+                src={DOST_SEAL}
+                alt="Phivolcs seal"
+                width={75}
+                height={75}
+                className="object-contain"
+            />
+            <Image 
+                src={PDRRMO_SEAL}
+                alt="PDRRMO seal"
+                width={75}
+                height={75}
+                className="object-contain"
+            />
+            <Image 
+                src={MDRRMO_SEAL}
+                alt="PDRRMO seal"
+                width={75}
+                height={75}
+                className="object-contain"
+            />
+            <Image 
+                src={BRGY_SEAL}
+                alt="BRGY seal"
+                width={75}
+                height={75}
+                className="object-contain"
+            />
+            <Image 
+                src={BRGY_SEAL}
+                alt="LEWC seal"
+                width={75}
+                height={75}
+                className="object-contain"
+            />
+        </Link>
+
+        {/* Mobile */}
+        <div className='sm:flex hidden'>
+            <button tyle="button" onClick={()=> { console.log("LOG ME HERE")}} className="outline_btn">
+                Sign in
+            </button>
+        </div>
+    </nav>
   )
 }
 
