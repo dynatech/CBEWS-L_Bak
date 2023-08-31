fd9c7406cbd5ae9aaef7b7523980de9581c785ef
John Geliberte <gelibertejohn@gmail.com>
2023-08-31 13:09:31 +0800

    + Finalized nav menu routes

---
+++ app/risk-profile/hazard-map/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HazardMap = () => {
+  return (
+    <div>HazardMap</div>
+  )
+}
+
+export default HazardMap
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -29,10 +29,11 @@ const Menu = (props) => {
                         </button>
                     </li>
                     <li>
-                        <Dropdown
+                        <Dropdown 
+                            className='pt-4 pb-2 dark:bg-primary-blue rounded-t-none'
                             renderTrigger={()=> {
                                 return (
-                                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-100 rounded hover:bg-white-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 md:w-auto dark:text-whitew dark:hover:text-primary-orange dark:focus:text-white dark:border-white md:hover:bg-transparent">
+                                    <button  className={`flex items-center py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 ${activeTab === 1 ? `md:text-primary-orange` : `md:text-white`} md:hover:text-primary-orange dark:hover:text-primary-orange md:hover:bg-transparent`}>
                                         Data Analysis
                                         <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
@@ -41,31 +42,28 @@ const Menu = (props) => {
                                 )
                             }}
                             >
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(1); router.push('/data-analysis'); }}>
                                 Summary
                             </Dropdown.Item>
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(1); router.push('/data-analysis/rainfall-graph'); }}>
                                 Rainfall Graph
                             </Dropdown.Item>
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(1); router.push('/data-analysis/surficial-graph'); }}>
                                 Surficial Graph
                             </Dropdown.Item>
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(1); router.push('/data-analysis/subsurface-graph'); }}>
                                 Subsurface Graph
                             </Dropdown.Item>
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(1); router.push('/data-analysis/earthquake-data'); }}>
                                 Earthquake Data
                             </Dropdown.Item>
                         </Dropdown>
                     </li>
                     <li>
-                        {/* <button onClick={() => { handleTabChange(2); router.push('/risk-profile'); }} className={`block py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 ${activeTab === 2 ? `md:text-primary-orange` : `md:text-white`} md:hover:text-primary-orange dark:hover:text-primary-orange md:hover:bg-transparent`}>
-                            Community Risk Assessment
-                        </button> */}
-                        <Dropdown
+                        <Dropdown className='pt-4 pb-2 dark:bg-primary-blue rounded-t-none'
                             renderTrigger={()=> {
                                 return (
-                                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-100 rounded hover:bg-white-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 md:w-auto dark:text-whitew dark:hover:text-primary-orange dark:focus:text-white dark:border-white md:hover:bg-transparent">
+                                    <button className={`flex items-center py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 ${activeTab === 2 ? `md:text-primary-orange` : `md:text-white`} md:hover:text-primary-orange dark:hover:text-primary-orange md:hover:bg-transparent`}>
                                         Community Risk Assessment
                                         <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
@@ -74,18 +72,34 @@ const Menu = (props) => {
                                 )
                             }}
                             >
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(2); router.push('/risk-profile/hazard-map'); }}>
                                 Hazard Map
                             </Dropdown.Item>
-                            <Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(2); router.push('/risk-profile/household-data'); }}>
                                 Household Data
                             </Dropdown.Item>
                         </Dropdown>
                     </li>
                     <li>
-                        <button onClick={()=> { handleTabChange(3); router.push('/ground-data'); }} className={`block py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 ${activeTab === 3 ? `md:text-primary-orange` : `md:text-white`} md:hover:text-primary-orange dark:hover:text-primary-orange md:hover:bg-transparent`}>
-                            Ground Data
-                        </button>
+                        <Dropdown className='pt-4 pb-2 dark:bg-primary-blue rounded-t-none'
+                            renderTrigger={()=> {
+                                return (
+                                    <button className={`flex items-center py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 ${activeTab === 3 ? `md:text-primary-orange` : `md:text-white`} md:hover:text-primary-orange dark:hover:text-primary-orange md:hover:bg-transparent`}>
+                                        Ground Data
+                                        <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                                        </svg>
+                                    </button>
+                                )
+                            }}
+                            >
+                            <Dropdown.Item onClick={() => { handleTabChange(3); router.push('/ground-data/surficial-markers'); }}>
+                                Surficial Markers
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={() => { handleTabChange(3); router.push('/ground-data/manifestation-of-movements'); }}>
+                                Manifestation of Movements
+                            </Dropdown.Item>
+                        </Dropdown>
                     </li>
                     <li>
                         <button onClick={()=> { handleTabChange(4); router.push('/communications'); }} className={`block py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-orange md:p-0 ${activeTab === 4 ? `md:text-primary-orange` : `md:text-white`} md:hover:text-primary-orange dark:hover:text-primary-orange md:hover:bg-transparent`}>
