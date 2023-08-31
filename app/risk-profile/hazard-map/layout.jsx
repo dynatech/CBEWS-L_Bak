8f2f51cfd0743b7dde344cf42cf80efb5502c71e
John Geliberte <gelibertejohn@gmail.com>
2023-08-31 14:04:47 +0800

    + Added hazard map

---
+++ app/risk-profile/hazard-map/layout.jsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => {
+    return (
+        <main className='flex justify-center items-center h-screen mx-16'>
+            {children}
+        </main>
+    )
+}
+
+export default Layout
--- app/risk-profile/hazard-map/page.jsx
+++ app/risk-profile/hazard-map/page.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+'use client';
 
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+import Map from '@public/maps/HazardMap.jpg';
 const HazardMap = () => {
+  const [screenWidth, setScreenWidth] = useState(null);
+
+  useEffect(() => {
+    function updateScreenWidth() {
+      setScreenWidth(window.innerWidth);
+    }
+
+    updateScreenWidth();
+    window.addEventListener('resize', updateScreenWidth);
+    return () => {
+      window.removeEventListener('resize', updateScreenWidth);
+    };
+  }, []);
+
   return (
-    <div>HazardMap</div>
+    <div className="w-full h-full pb-20">
+      <div className="text-center pt-5">
+        <div className="flex flex-col justify-center items-center drop-shadow-lg gap-6">
+          <Image 
+              src={Map}
+              alt="Dynaslope seal"
+              width={screenWidth-(screenWidth*.4)}
+              height={screenWidth-(screenWidth*.4)}
+              className="object-contain shadow-lg"
+          />
+          <div>
+            <button tyle="button" className="text-white bg-primary-blue rounded-full p-4 flex-center" 
+              onClick={()=> {
+
+              }}>Download Hazard Map
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
