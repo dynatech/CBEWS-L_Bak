6fc0ff5e54e8156b42825bb765f9c73e811f3703
John Geliberte <gelibertejohn@gmail.com>
2023-09-05 17:50:41 +0800

    + Changed hover

--- components/ExperimentalTable.jsx
+++ components/ExperimentalTable.jsx
@@ -63,7 +63,7 @@ const ExperimentalTable = ({
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="bg-gray-200 dark:bg-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-yellow-500 dark:text-gray-700 dark:hover:text-white">
+                    <tr class="bg-gray-200 dark:bg-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-yellow-500 dark:text-gray-700 dark:hover:text-gray-100">
                         <td class="w-4 p-4 border-r-2 border-gray-400 border-b-2">
                             <div class="flex items-center">
                                 <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-500 bg-gray-100 border-gray-100 rounded focus:ring-primary-orange dark:focus:ring-primary-orange dark:ring-primary-orange dark:focus:ring-offset-primary-orange focus:ring-1 dark:bg-gray-white dark:border-gray-700" />
@@ -84,14 +84,14 @@ const ExperimentalTable = ({
                         </td>
                         <td class="px-auto py-4 border-r-2 border-gray-400 border-b-2">
                             <div className='flex gap-6 text-center justify-center'>
-                                <svg class="w-6 h-6 text-gray-800 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-primary-blue hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                                     <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"/>
                                     <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z"/>
                                 </svg>
-                                <svg class="w-6 h-6 text-gray-800 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-primary-blue hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
                                 </svg>
-                                <svg class="w-6 h-6 text-gray-800 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 10">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-primary-blue hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M6 1h10M6 5h10M6 9h10M1.49 1h.01m-.01 4h.01m-.01 4h.01"/>
                                 </svg>
                             </div>
@@ -118,14 +118,14 @@ const ExperimentalTable = ({
                         </td>
                         <td class="px-auto py-4 border-r-2 border-gray-400 border-b-2">
                             <div className='flex gap-6 text-center justify-center'>
-                                <svg class="w-6 h-6 text-gray-800 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-primary-blue hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                                     <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"/>
                                     <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z"/>
                                 </svg>
-                                <svg class="w-6 h-6 text-gray-800 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-primary-blue hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
                                 </svg>
-                                <svg class="w-6 h-6 text-gray-800 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 10">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-primary-blue hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M6 1h10M6 5h10M6 9h10M1.49 1h.01m-.01 4h.01m-.01 4h.01"/>
                                 </svg>
                             </div>
