70af4907c174e95330597888730dd03ceee32524
John Geliberte <gelibertejohn@gmail.com>
2023-09-05 16:55:56 +0800

    + Added experimental table with search and pagination

--- app/ground-data/surficial-markers/page.jsx
+++ app/ground-data/surficial-markers/page.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+'use client';
+import Tables from '@components/Tables';
+import ExperimentalTable from '@components/ExperimentalTable';
 
 const SurficialMarkers = () => {
+
+  const TABLE_HEADERS = [
+    "Date",
+    "Time",
+    "A",
+    "B",
+    "C",
+    "D",
+    "Nag-sukat"
+  ];
+
+  const TABLE_CONTENT = [];
+
   return (
-    <div>SurficialMarkers</div>
+    <div className="w-full pb-20">
+      <div className="text-center pt-5">
+        <div className="container mx-auto px-10">
+          <div>
+            <h4 className="font-bold text-primary-blue-100 text-4xl py-4 text-left">
+              Surficial Markers
+            </h4>
+            {/* <Tables
+              headers={TABLE_HEADERS}
+              content={TABLE_CONTENT}
+              
+              actions={() => { }} /> */}
+
+              <ExperimentalTable/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
