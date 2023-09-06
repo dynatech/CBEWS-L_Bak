8154c32252e82bb436125f682a8f5f34738afa58
John Geliberte <gelibertejohn@gmail.com>
2023-09-06 14:23:58 +0800

    + Fixed table generation using dynamic data

--- app/ground-data/surficial-markers/page.jsx
+++ app/ground-data/surficial-markers/page.jsx
@@ -4,17 +4,120 @@ import ExperimentalTable from '@components/ExperimentalTable';
 
 const SurficialMarkers = () => {
 
-  const TABLE_HEADERS = [
-    "Date",
-    "Time",
-    "A",
-    "B",
-    "C",
-    "D",
-    "Nag-sukat"
-  ];
+  const DummySurficial = {
+    headers: [
+      "Date",
+      "Time",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "Nag-sukat"
+    ],
+    data: [
+      {
+        date: "September 6, 2023",
+        time: "07:30 AM",
+        A: "0",
+        B: "0",
+        C: "0",
+        D: "0",
+        E: "0",
+        F: "0",
+        G: "0",
+        H: "0",
+        I: "0",
+        J: "0",
+        measurer: "John Geliberte"
+      },
+      {
+        date: "September 6, 2023",
+        time: "07:30 AM",
+        A: "0",
+        B: "0",
+        C: "0",
+        D: "0",
+        E: "0",
+        F: "0",
+        G: "0",
+        H: "0",
+        I: "0",
+        J: "0",
+        measurer: "David guevarra"
+      },
+      {
+        date: "September 6, 2023",
+        time: "07:30 AM",
+        A: "0",
+        B: "0",
+        C: "0",
+        D: "0",
+        E: "0",
+        F: "0",
+        G: "0",
+        H: "0",
+        I: "0",
+        J: "0",
+        measurer: "Philline Efondo"
+      },
+      {
+        date: "September 6, 2023",
+        time: "07:30 AM",
+        A: "0",
+        B: "0",
+        C: "0",
+        D: "0",
+        E: "0",
+        F: "0",
+        G: "0",
+        H: "0",
+        I: "0",
+        J: "0",
+        measurer: "Carla Joy Orubia"
+      },
+      {
+        date: "September 6, 2023",
+        time: "07:30 AM",
+        A: "0",
+        B: "0",
+        C: "0",
+        D: "0",
+        E: "0",
+        F: "0",
+        G: "0",
+        H: "0",
+        I: "0",
+        J: "0",
+        measurer: "Jeremiah de guzman"
+      }
+    ]
+  }
 
-  const TABLE_CONTENT = [];
+  const handleUpdate = () => {
+    console.log("HANDLE UPDATE")
+  }
+
+  const handleDelete = () => {
+    console.log("HANDLE DELETE")
+  }
+
+  const handleView = () => {
+    console.log("HANDLE VIEW")
+  }
+
+  const handleActions = () => {
+    return {
+      handleUpdate,
+      handleDelete,
+      handleView
+    }
+  }
 
   return (
     <div className="w-full pb-20">
@@ -24,13 +127,9 @@ const SurficialMarkers = () => {
             <h4 className="font-bold text-primary-blue-100 text-4xl py-4 text-left">
               Surficial Markers
             </h4>
-            {/* <Tables
-              headers={TABLE_HEADERS}
-              content={TABLE_CONTENT}
-              
-              actions={() => { }} /> */}
-
-              <ExperimentalTable/>
+            <ExperimentalTable content={DummySurficial}
+              actions={handleActions}
+            />
           </div>
         </div>
       </div>
